53ebcced7ca795c395412e01bb532a8c5a7e5330
Roel Willems <git@roelwillems.com>
2015-02-22 16:24:18 +0100

    Initial commit

---
+++ x/framework/views/global/_header.php
@@ -0,0 +1,39 @@
+<?php
+
+// =============================================================================
+// VIEWS/GLOBAL/_HEADER.PHP
+// -----------------------------------------------------------------------------
+// Declares the DOCTYPE for the site and include the <head>.
+// =============================================================================
+
+?>
+
+<!DOCTYPE html>
+<!--[if IE 9]><html class="no-js ie9" <?php language_attributes(); ?>><![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js" <?php language_attributes(); ?>><!--<![endif]-->
+
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php wp_title(''); ?></title>
+  <link rel="profile" href="http://gmpg.org/xfn/11">
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+  <?php wp_head(); ?>
+  <!-- Google Tag Manager -->
+  <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-K6RGXF"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-K6RGXF');</script>
+  <!-- End Google Tag Manager -->
+</head>
+
+<body <?php body_class(); ?>>
+
+  <?php do_action( 'x_before_site_begin' ); ?>
+
+  <div id="top" class="site">
+
+  <?php do_action( 'x_after_site_begin' ); ?>
